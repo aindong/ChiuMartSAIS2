728d1e636221610ce9fcd808d81a485dbecb71e5
Alleo Indong <freedom_war550@live.com.ph>
2014-10-28 07:00:25 +0800

    Created the design for stocks conversion

---
+++ ChiuMartSAIS2/App/Dialogs/dlgConversionForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace ChiuMartSAIS2.App.Dialogs
+{
+    public partial class dlgConversionForm : Form
+    {
+        public dlgConversionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dlgConversionForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Update the stocks
+        }
+    }
+}
---
+++ ChiuMartSAIS2/App/frmChiumartRetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace ChiuMartSAIS2.App
+{
+    public partial class frmChiumartRetail : Form
+    {
+        public frmChiumartRetail()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Dialogs.dlgConversionForm dlg = new Dialogs.dlgConversionForm();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // Insert the new conversion transaction/Update the database stock
+            }
+        }
+    }
+}
